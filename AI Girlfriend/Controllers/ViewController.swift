339b1bba5c3dcf123f219d738cc86eb0da561525
JangWoojun <woojun0107@naver.com>
2023-03-10 19:13:59 +0900

    feat: MainView 설정

---
+++ AI Girlfriend/Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let mainView = MainView()
+    
+    override func loadView() {
+        view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
