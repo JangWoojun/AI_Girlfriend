66b225123e92d0e00c0ee83b9ae0e5dc48c233a7
JangWoojun <woojun0107@naver.com>
2023-03-13 19:38:43 +0900

    feat: TalkViewController 생성

---
+++ AI Girlfriend/Controllers/TalkViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TalkViewController.swift
+//  AI Girlfriend
+//
+//  Created by Jangwoojun on 2023/03/13.
+//
+
+import UIKit
+
+class TalkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
