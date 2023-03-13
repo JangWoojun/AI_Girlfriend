912079679822c07ce12e6b1035697a5eeec48b07
JangWoojun <woojun0107@naver.com>
2023-03-13 21:19:29 +0900

    feat: mainLabel 설정

--- AI Girlfriend/Views/TalkView.swift
+++ AI Girlfriend/Views/TalkView.swift
@@ -1,11 +1,30 @@
 import UIKit
 
 class TalkView: UIView {
+    
+    let mainLabel: UILabel = {
+        let tv = UILabel()
+        tv.textColor = .black
+        tv.font = .boldSystemFont(ofSize: 26)
+        tv.text = "메시지"
+
+        return tv
+    }()
+    
+    func setUI() {
+        self.addSubview(mainLabel)
+        self.backgroundColor = .white
+        
+        mainLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mainLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 10),
+            mainLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 20)
+        ])
+    }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        self.backgroundColor = .white
+        setUI()
     }
 
     required init?(coder: NSCoder) {
