3be0c21b5484e4f786dc80bab56e1557c39f8f69
JangWoojun <woojun0107@naver.com>
2023-03-13 21:17:56 +0900

    feat: Background 색 설정

--- AI Girlfriend/Views/MainView.swift
+++ AI Girlfriend/Views/MainView.swift
@@ -4,6 +4,8 @@ class MainView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        self.backgroundColor = .white
     }
 
     required init?(coder: NSCoder) {
