2aacfb2a9d562f2583fc9961c07622117630102b
JangWoojun <woojun0107@naver.com>
2023-03-14 22:24:46 +0900

    feat: 기본 세팅

--- AI Girlfriend/Controllers/TalkChatViewController.swift
+++ AI Girlfriend/Controllers/TalkChatViewController.swift
@@ -1,6 +1,12 @@
 import UIKit
 
 class TalkChatViewController: UIViewController {
+    
+    private let talkChatView = TalkChatView()
+    
+    override func loadView() {
+        view = talkChatView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
