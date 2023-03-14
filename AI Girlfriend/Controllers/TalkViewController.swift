50c760956378c072cce09bc7e3157e1708cd1f57
JangWoojun <woojun0107@naver.com>
2023-03-14 21:21:39 +0900

    feat TableView 작업 완료

--- AI Girlfriend/Controllers/TalkViewController.swift
+++ AI Girlfriend/Controllers/TalkViewController.swift
@@ -10,17 +10,60 @@ import UIKit
 class TalkViewController: UIViewController {
     
     private let talkView = TalkView()
-    
+    private let tableView = UITableView()
+
     override func loadView() {
         view = talkView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        tableView.rowHeight = 80
+        tableView.separatorStyle = .none
+
+        tableView.register(TalkTableViewCell.self, forCellReuseIdentifier: "GirlFriendCell")
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: talkView.mainLabel.bottomAnchor, constant: 20),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
     }
     
+}
 
+extension TalkViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GirlFriendCell", for: indexPath) as! TalkTableViewCell
+        cell.selectionStyle = .none
 
+        return cell
+    }
+    
+}
+
+extension TalkViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let nextVC = ViewController()
+
+        navigationController?.pushViewController(nextVC, animated: true)
+    }
 }
