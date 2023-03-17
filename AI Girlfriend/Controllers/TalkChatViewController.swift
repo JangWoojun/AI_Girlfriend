5e013cc204003c7003c1ac01d450d80d69ccf95a
JangWoojun <woojun0107@naver.com>
2023-03-17 17:20:41 +0900

    feat: MessageKit 기본 화면 설정

--- AI Girlfriend/Controllers/TalkChatViewController.swift
+++ AI Girlfriend/Controllers/TalkChatViewController.swift
@@ -1,17 +1,51 @@
 import UIKit
 import MessageKit
 
-class TalkChatViewController: UIViewController {
+struct Sender: SenderType {
+    var senderId: String
+    var displayName: String
+}
+
+struct Message: MessageType {
+    var sender: MessageKit.SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKit.MessageKind
+}
+
+class TalkChatViewController: MessagesViewController, MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    
+    let currentUser = Sender(senderId: "self", displayName: "나")
+    let otherUser = Sender(senderId: "other", displayName: "여친❤")
+    var messages =  [MessageType]()
     
     private let talkChatView = TalkChatView()
     
     override func loadView() {
         view = talkChatView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        
     }
     
-
+    func currentSender() -> MessageKit.SenderType {
+        return currentUser
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessageKit.MessagesCollectionView) -> MessageKit.MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessageKit.MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
+    
 }
+
