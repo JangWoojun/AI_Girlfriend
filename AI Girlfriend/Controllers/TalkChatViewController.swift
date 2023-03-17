fafb11aeede94e81dda40165b5e0834a178676d6
JangWoojun <woojun0107@naver.com>
2023-03-17 17:36:35 +0900

    feat: 전송 기능 추가

--- AI Girlfriend/Controllers/TalkChatViewController.swift
+++ AI Girlfriend/Controllers/TalkChatViewController.swift
@@ -1,5 +1,7 @@
 import UIKit
 import MessageKit
+import InputBarAccessoryView
+
 
 struct Sender: SenderType {
     var senderId: String
@@ -13,7 +15,7 @@ struct Message: MessageType {
     var kind: MessageKit.MessageKind
 }
 
-class TalkChatViewController: MessagesViewController, MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+class TalkChatViewController: MessagesViewController, MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate, InputBarAccessoryViewDelegate {
     
     let currentUser = Sender(senderId: "self", displayName: "나")
     let otherUser = Sender(senderId: "other", displayName: "여친❤")
@@ -28,13 +30,27 @@ class TalkChatViewController: MessagesViewController, MessagesDataSource, Messag
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        messageInputBar.delegate = self
         messagesCollectionView.messagesDataSource = self
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDisplayDelegate = self
+        messageInputBar.inputTextView.placeholder = "메시지를 입력하세요."
+        messageInputBar.sendButton.title = "전송"
+        messageInputBar.sendButton.isEnabled = false
         
-        messages.append(Message(sender: otherUser, messageId: "1", sentDate: Date().addingTimeInterval(-86400), kind: .text("오늘은 뭐하고 왔어 OO아?")))
+        messages.append(Message(sender: otherUser, messageId: "1", sentDate: Date().addingTimeInterval(-86400), kind: .text("오늘은 뭐하고 왔어 성준아?")))
         
-        messages.append(Message(sender: currentUser, messageId: "3", sentDate: Date().addingTimeInterval(-86400), kind: .text("난")))
+        let layout = messagesCollectionView.collectionViewLayout as? MessagesCollectionViewFlowLayout
+        layout?.setMessageOutgoingAvatarSize(.zero)
+    }
+    
+    func sendButtonClicked() {
+        guard let text = messageInputBar.inputTextView.text else { return }
+        let message = Message(sender: currentUser, messageId: "self", sentDate: Date(), kind: .text(text))
+        messages.append(message)
+        messagesCollectionView.reloadData()
+        messageInputBar.inputTextView.text = ""
+        messageInputBar.invalidatePlugins()
     }
     
     func currentSender() -> MessageKit.SenderType {
@@ -48,6 +64,11 @@ class TalkChatViewController: MessagesViewController, MessagesDataSource, Messag
     func numberOfSections(in messagesCollectionView: MessageKit.MessagesCollectionView) -> Int {
         return messages.count
     }
+    
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+        sendButtonClicked()
+    }
+    
     
     
 }
